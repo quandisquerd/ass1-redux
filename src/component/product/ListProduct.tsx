7779f15e39de35b18190efabb9b2eac78e1fda84
quan1234 <quanvdph19781@fpt.edu.vn>
2023-07-25 15:49:39 +0700

    ass1 redux

---
+++ src/component/product/ListProduct.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from "react-redux"
+import { add, deletePro, fetch, update } from '../../action/product';
+
+
+
+
+const ListProduct = () => {
+    const dispatch = useDispatch<any>()
+    const { products } = useSelector((state: any) => state.products)
+    const [data, setdasta] = useState([])
+    useEffect(() => {
+        dispatch(fetch())
+    }, [])
+    return (
+        <>
+            <form onSubmit={dispatch(() => add({ name: data }))}>
+                <input type="text" onChange={(e: any) => setdasta(e.target.value)} />
+                <button>add</button>
+            </form>
+            {products?.map((data: any) => {
+                return <div key={data.id}>
+                    <h1>{data.name}</h1>
+                    <button onClick={() => dispatch(deletePro(data.id))}>xoa</button>
+                    <button onClick={() => dispatch(update({ name: "bo quan", id: data.id }))}>sua</button>
+                </div>
+            })}
+        </>
+    )
+}
+
+export default ListProduct
